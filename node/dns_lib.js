3b47e4aca7c8f92822dc9be1f5a428243cf98e7c
stevewooo <461437874@qq.com>
2019-05-28 18:21:54 +0800

    init

--- node/dns_lib.js
+++ node/dns_lib.js
@@ -1,7 +1,7 @@
 /**
 * 创建头部信息
 */
-function buildHeader(bits){
+function buildHeader(options, bits){
 	/**
 	* 1、创建序列id(2bit)
 	*/
@@ -17,6 +17,7 @@ function buildHeader(bits){
 
 	/**
 	* 3、询问数目
+	* 查一个域名，所以丢一个1进去
 	*/
 	bits.push(0);
 	bits.push(1);
@@ -36,22 +37,73 @@ function buildHeader(bits){
 	return bits;
 }
 
-exports.buildPackage = function(options){
-	var buf;
-	buf = Buffer.alloc(1024);
+/**
+* 塞域名进去
+*/
+function buildQuestion(options, bits){
+	var domain = options.domain;
 
+	domain = domain.split('.');
+	for(var i=0;i<domain.length;i++){
+		bits.push(domain[i].length); //长度计数丢进去咯
+
+		/**
+		* 然后把每个字符串转换成ascii码再丢进去
+		*/
+		for(var k=0;k<domain[i].length;k++){
+			bits.push(domain[i][k].charCodeAt());
+		}
+	}
+	//别忘了加个0结束；
+	bits.push(0);
+
+	return bits;
+}
+
+/**
+* 塞类型进去（两个1）
+*/
+function buildType(options, bits){
+	/**
+	* type 1代表A解析
+	*/
+	bits.push(0);
+	bits.push(1);
+
+	/**
+	* classes
+	*/
+	bits.push(0);
+	bits.push(1);
+
+	return bits;
+}
+
+/**
+* 把bits转换成ascii字符，一个一个传到buf中去
+*/
+function convertBitsToBuffer(options, bits){
+	var buf = Buffer.alloc(bits.length);
+	for(var i=0;i<bits.length;i++){
+		var char = String.fromCharCode(bits[i]);
+		buf.write(char, i); //或者用concat(buf1, buf2)合起来新的buf；
+	}
+	return buf;
+}
+
+exports.buildPackage = function(options){
 	/**
 	* 因为Buffer.write是要传一个字符串，然后他再转换成ascii码，然后再变成二进制码发出去
 	* 所以这里要把他们先弄成一个一个bit，放进bits
 	* bits中每个bit代表期望放进最终buffer中的每个bit 的ascii码。
+	* 例如你想弄一位 | 0000 1000 | 进去buffer中，那么就 bits.push(8);
 	*/
 	var bits = [];
+	bits = buildHeader(options, bits);
+	bits = buildQuestion(options, bits);
+	bits = buildType(options, bits);
 
-	console.log(buf);
-
-	bits = buildHeader(bits);
-
-	console.log(buf);
+	var buf = convertBitsToBuffer(options, bits);
 
 	return buf;
 }
--- node/send_dns_package.js
+++ node/send_dns_package.js
@@ -1,17 +1,30 @@
 const dgram = require('dgram');
+const path = require('path');
 const client = dgram.createSocket('udp4');
-const dnsLib = require('./dns_lib');
+const dnsLib = require(`${path.resolve()}/dns_lib`);
+
+async function initUdp(){
+	client.on('message', (msg, info)=>{
+		console.log(msg);
+		console.log(`result ip : ${msg[msg.length-4]}.${msg[msg.length-3]}.${msg[msg.length-2]}.${msg[msg.length-1]}`);
+	})
+
+	client.on('error', (err) => {
+		console.log(err);
+	});
+}
 
 async function main(){
-	var buf = dnsLib.buildPackage({});
+	await initUdp();
+	var buf = dnsLib.buildPackage({
+		domain : "hongkong.com"
+	});
+
+	console.log(buf);
+	client.send(buf, 53, '127.0.0.1', (error)=>{
+		if(error){
+			console.log(error);
+		}
+	})
 }
 main();
-
-
-// client.send(buf, 53, '127.0.0.1', (info)=>{
-// 	// console.log(info);
-// })
-
-// client.on('message', (msg, info)=>{
-// 	// console.log(msg.toString());
-// })
