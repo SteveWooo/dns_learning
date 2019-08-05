e48bfe69b6e5729cb49c79217e5321c4f7d81528
461437874@qq.com <461437874@qq.com>
2019-08-05 16:07:47 +0800

    add cia server

--- ciaserver/startup.js
+++ ciaserver/startup.js
@@ -14,11 +14,12 @@ function getClientIp(req) {
     return ip;  
 };
 
-app.get('/', async function (req,res){
-	var ip = getClientIp(req);
-	console.log('request : ' + ip);
-	res.send(
-`
+async function startupServer(swc){
+    app.get('/', async function (req,res){
+    var ip = getClientIp(req);
+    console.log('request : ' + ip);
+    res.send(
+    `
 <html>
 <head>
 </head>
@@ -28,7 +29,75 @@ app.get('/', async function (req,res){
 </script>
 </body>
 </html>
-`);
-})
+    `);
+    })
 
-app.listen(80);
+    app.listen(80);
+}
+
+async function getConfig(){
+    var swc = {};
+    // 提取元数据
+    var metaData = fs.readFileSync('/meta.js').toString();
+
+    try {
+        metaData = JSON.parse(metaData);
+        swc.meta = {};
+        for(var i in metaData){
+            var t = i.split('.');
+            if(t[0] == 'dns'){
+                swc.meta[t[1]] = metaData[i]; 
+            }
+        }
+
+        //递归服务器不需要配置zone
+        if(swc.meta.serverType == 'recursive'){
+            return swc;
+        }
+
+        if(swc.meta.serverType == 'authoritative'){
+            //权威服务器必须传IP
+            if(swc.meta.ip == undefined){
+                console.log('缺少ip meta配置');
+                return undefined;
+            }
+
+            // 获取本机所需管理的zone
+            if(swc.meta.zones == undefined){
+                var config = require('/etc/named/zone/zoneFiles/config.json');
+                for(var i=0;i<config.length;i++){
+                    for(var s=0;s<config[i].servers.length;s++){
+                        if(config[i].servers[s].ip == swc.meta.ip) {
+                            swc.meta.zones = config[i].servers[s].zones
+                        }
+                    }
+                }
+            }
+
+            if(swc.meta.zones == undefined){
+
+                return undefined;
+            }
+
+            return swc;
+        }
+
+        //no match
+        return undefined;
+    }catch(e){
+        console.log(e);
+    }
+}
+
+async function main(){
+    var swc = await getConfig();
+    if(swc != undefined){
+        if(swc.meta.serverType == 'cia'){
+            await startupServer(swc);
+        }
+    }
+
+    console.log('cid end');
+}
+
+main();
